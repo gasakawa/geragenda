81fef763e75931814bb92d4aa296522cd153acf8
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2024-01-02 17:37:57 -0300

    test: add unit tests to forgot password service

--- src/auth/services/forgot-password.service.spec.ts
+++ src/auth/services/forgot-password.service.spec.ts
@@ -4,6 +4,7 @@ import { UserRepository } from '@/core/infra/db/repositories/user.repository';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionMock } from '@/core/infra/db/repositories/__mocks__/base-repository.mock';
 import { faker } from '@faker-js/faker';
+import { UserEntity } from '@/core/domain/entities/user.entity';
 
 jest.mock('@/core/infra/db/repositories/user.repository');
 
@@ -47,6 +48,50 @@ describe('ForgotPasswordService', () => {
 
     expect(userRepo.commit).toHaveBeenCalled();
     expect(userRepo.rollback).not.toHaveBeenCalled();
+    expect(userRepo.findOne).toHaveBeenCalled();
+    expect(cognitoProvider.forgotPassword).not.toHaveBeenCalled();
+  });
+
+  it('should call forgot password', async () => {
+    jest
+      .spyOn(userRepo, 'findOne')
+      .mockResolvedValueOnce({ sub: faker.string.uuid() } as UserEntity);
+
+    jest.spyOn(userRepo, 'startTrx').mockResolvedValueOnce(fakeTx);
+    jest.spyOn(userRepo, 'commit').mockResolvedValueOnce(undefined);
+    jest.spyOn(userRepo, 'rollback').mockResolvedValueOnce(undefined);
+    jest
+      .spyOn(cognitoProvider, 'forgotPassword')
+      .mockResolvedValueOnce(undefined);
+
+    await sut.execute(faker.internet.email());
+
+    expect(userRepo.commit).toHaveBeenCalled();
+    expect(userRepo.rollback).not.toHaveBeenCalled();
+    expect(userRepo.findOne).toHaveBeenCalled();
+    expect(cognitoProvider.forgotPassword).toHaveBeenCalled();
+  });
+
+  it('should rollback if an error occurs', async () => {
+    jest
+      .spyOn(userRepo, 'findOne')
+      .mockRejectedValueOnce(new Error('an error'));
+
+    jest.spyOn(userRepo, 'startTrx').mockResolvedValueOnce(fakeTx);
+    jest.spyOn(userRepo, 'commit').mockResolvedValueOnce(undefined);
+    jest.spyOn(userRepo, 'rollback').mockResolvedValueOnce(undefined);
+    jest
+      .spyOn(cognitoProvider, 'forgotPassword')
+      .mockResolvedValueOnce(undefined);
+
+    try {
+      await sut.execute(faker.internet.email());
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+
+    expect(userRepo.commit).not.toHaveBeenCalled();
+    expect(userRepo.rollback).toHaveBeenCalled();
     expect(userRepo.findOne).toHaveBeenCalled();
     expect(cognitoProvider.forgotPassword).not.toHaveBeenCalled();
   });
